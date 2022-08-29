d2e02e1bc54fdd96d5c7ea9e54746196ba819948
ad105geppetto <ad105geppetto@gmail.com>
2022-08-29 20:56:56 +0900

    [feat] Add login form

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 const Auth = () => {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          onChange={onChange}
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+        />
+        <input
+          name="password"
+          onChange={onChange}
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
